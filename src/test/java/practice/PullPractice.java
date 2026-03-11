64e49f2b6eba0ab1bf926b3626b47b8b61a23828
jayas <jayas@Jayashree>
2026-03-11 13:03:50 +0530

    ATE1 branch

--- src/test/java/practice/PullPractice.java
+++ src/test/java/practice/PullPractice.java
@@ -4,8 +4,8 @@ public class PullPractice {
 
 	public static void main(String[] args) 
 	{
-		 System.out.println("New class for pull is craeted");
-		 System.out.println("pushing through  ATE1 new branch");
+		 System.out.println("New class for pull is created");
+		 System.out.println("pushing through  ATE1 ");
 		 
 
 	}
