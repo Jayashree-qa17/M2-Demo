4e291beb7bc85149972e997e185c780cfbedf9bd
jayas <jayas@Jayashree>
2026-03-12 12:12:56 +0530

    Conflict from ATE

--- src/test/java/practice/PullPractice.java
+++ src/test/java/practice/PullPractice.java
@@ -7,7 +7,7 @@ public class PullPractice {
 		 System.out.println("New class for pull is created");
 		 System.out.println("pushing through  ATE1 ");
 		 System.out.println("second branch creation ATE2");
-		 System.out.println("Git conflict from ATE");
+		 System.out.println("Git conflict from ATE1");
 		 
 
 	}
